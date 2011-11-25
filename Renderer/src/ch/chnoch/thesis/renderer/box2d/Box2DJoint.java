c0f24d67cab718c425f84a9d2b7748a4078830c5
Chnoch <bas@students.unibe.ch>
2011-11-25 13:51:02 +0100

    merge

--- Renderer/src/ch/chnoch/thesis/renderer/box2d/Box2DJoint.java
+++ Renderer/src/ch/chnoch/thesis/renderer/box2d/Box2DJoint.java
@@ -11,25 +11,25 @@ import org.jbox2d.dynamics.joints.MouseJointDef;
 
 public class Box2DJoint {
 	private Joint mJoint;
-	private DistanceJointDef mJointDef;
+	private MouseJointDef mJointDef;
 
 	public Box2DJoint(Box2DBody body1, Box2DWorld world) {
-		mJointDef = new DistanceJointDef();
-		mJointDef.body1 = body1.getBody();
-		mJointDef.body2 = world.getTopBody().getBody();
+		mJointDef = new MouseJointDef();
+		mJointDef.body2 = body1.getBody();
+		mJointDef.body1 = world.getGroundBody().getBody();
 		mJointDef.collideConnected = true;
 		mJointDef.type = JointType.DISTANCE_JOINT;
-		mJointDef.localAnchor1 = new Vec2(body1.getCurrentPosition().x, body1.getCurrentPosition().y);
-		mJointDef.localAnchor2 = new Vec2(world.getGroundBody().getCurrentPosition().x, world.getGroundBody().getCurrentPosition().y);
-		mJointDef.length = mJointDef.localAnchor1.sub(mJointDef.localAnchor2).length();
+//		mJointDef.localAnchor1 = new Vec2(body1.getCurrentPosition().x, body1.getCurrentPosition().y);
+//		mJointDef.localAnchor2 = new Vec2(world.getGroundBody().getCurrentPosition().x, world.getGroundBody().getCurrentPosition().y);
+//		mJointDef.length = mJointDef.localAnchor1.sub(mJointDef.localAnchor2).length();
 		mJoint = world.createJoint(this);
 	}
 
 	public void update(Vec2 newTarget) {
-//		if (mJoint instanceof MouseJoint) {
-//			((DistanceJoint) mJoint).setTarget(newTarget);
-//		}
-		mJointDef.localAnchor1 = newTarget;
+		if (mJoint instanceof MouseJoint) {
+			((MouseJoint) mJoint).setTarget(newTarget);
+		}
+//		mJointDef.localAnchor1 = newTarget;
 	}
 
 	JointDef getJointDef() {
